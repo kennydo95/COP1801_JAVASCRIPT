a00f1c879c2ee81afb2dbf20ad1e35e56691ce45
kennydo95 <kennydo77@gmail.com>
2025-02-16 20:54:29 -0600

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var heading = document.createElement("h1");
+    heading.textContent = "Welcome to my website!";
+    document.body.appendChild(heading);
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var paragraph = document.createElement("p");
+    paragraph.textContent = `My name is Kenny Do, currenly I am pursuing Software Development. 
+        I'm taking this course to learn more about JavaScript and how to use it in web development.
+        Thank you for visiting my page.`;
+    document.body.appendChild(paragraph);
+});
