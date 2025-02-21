5167660cd538a59413d06b55294c669187a7fc5c
kennydo95 <kennydo77@gmail.com>
2025-02-20 23:21:06 -0600

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,12 +1,25 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var heading = document.createElement("h1");
-    heading.textContent = "Welcome to my website!";
-    document.body.appendChild(heading);
-});
-document.addEventListener("DOMContentLoaded", function() {
-    var paragraph = document.createElement("p");
-    paragraph.textContent = `My name is Kenny Do, currenly I am pursuing Software Development. 
-        I'm taking this course to learn more about JavaScript and how to use it in web development.
-        Thank you for visiting my page.`;
-    document.body.appendChild(paragraph);
-});
+//Ask the user to enter their first name
+let Fname = prompt("Please enter your first name:");
+
+//Check if the user entered a name
+if (Fname){
+    document.getElementById("welcomeMessage").innerText = `Welcome, ${Fname}! Nice to have you here!`;
+} else {
+    document.getElementById("welcomeMessage").innerText = "Welcome! Nice to have you here!";
+}
+
+//Constant piValue
+const piValue = 3.1415926;
+
+//Ask the user to enter their fav numbers
+let favNum = prompt("Please enter your first favorite number:");
+
+//Check if the user entered a number
+if (favNum){
+    document.getElementById("favNum").innerText = `Your first favorite number is ${favNum}.`;
+} else {
+    document.getElementById("favNum").innerText = "You did not enter a number.";
+}
+
+let myArea = piValue * (favNum * favNum);
+document.getElementById("area").innerText = `Your favorite number is ${favNum}, if that number was the radius area of a circle that could ${myArea}.`;
